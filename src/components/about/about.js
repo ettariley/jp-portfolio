15cc4c690f635751dcc1417b0adfeb7bf9be4d33
Etta Baker <etta.r.baker@gmail.com>
2022-07-11 00:43:31 -0400

    Created about page and css file

---
+++ src/components/about/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import './about.css';
+
+function About() {
+  return (
+    <Container fluid className='about'>
+        This is the about page.
+    </Container>
+  );
+}
+
+export default About;
