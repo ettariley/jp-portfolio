4fd7d5de7809f623f4ef50bd8a3f0bca015dc1cb
Etta Baker <etta.r.baker@gmail.com>
2022-07-11 00:43:48 -0400

    Created footer and css file

---
+++ src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import './footer.css';
+
+function Footer() {
+  return (
+    <Container fluid className='footer'>
+        This is the footer.
+    </Container>
+  );
+}
+
+export default Footer;
