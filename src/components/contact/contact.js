aeafa0379629f775b98318bd345f8abec5d2c6ec
Etta Baker <etta.r.baker@gmail.com>
2022-07-11 00:43:39 -0400

    Created contact page and css file

---
+++ src/components/contact/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import './contact.css';
+
+function Contact() {
+  return (
+    <Container fluid className='contact'>
+      <h2>Contact</h2>
+      This is the contact page.
+    </Container>
+  );
+}
+
+export default Contact;
