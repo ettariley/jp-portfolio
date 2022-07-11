acd7a975119ec9314c7e9ab822a614a2751e8247
Etta Baker <etta.r.baker@gmail.com>
2022-07-11 00:44:19 -0400

    Created work page and css file

---
+++ src/components/work/work.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import './work.css';
+
+function Work() {
+  return (
+    <Container fluid className='work'>
+        This is the work page.
+    </Container>
+  );
+}
+
+export default Work;
