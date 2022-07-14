bbee247a06885ed4fbcb5140a27e90e88097ec29
Etta Baker <etta.r.baker@gmail.com>
2022-07-13 23:34:25 -0400

    Updated icons, stylings, and fixed a typo

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -15,24 +15,24 @@ function Home() {
           </Row>
           <Row className='d-flex justify-content-center middle-content'>
             <Column lg='5' md='6'>
-              <Image className='img-fluid' roundedCircle='true' src={require('../../assets/headshot.png')} />
+              <Image className='img-fluid' roundedCircle='true' src={require('../../assets/headshot.jpg')} />
             </Column>
             <Column lg='5' md='6'>
               <h5>Full-Stack Web Developer in Bristol, Tennessee</h5>
-              <p>With a background in social work and human resources, I've bring a person-first perspective to everything I do.</p>
-              <p>I'm open to working remote or locally in a developer position within a company that aims to make a positive impact on both its employees and its users.</p>
+              <p>With a background in social work and human resources, I bring a person-first perspective to everything I do.</p>
+              <p>I'm open to working remote or locally in a developer position within a company that aims to make a positive impact on both its employees and its users. I'm open to freelance work as well.</p>
               <p>Interested? Check out my GitHub, LinkedIn, or Resume below. See my work, learn more about me, or contact me at the links above.</p>              
             </Column>
           </Row>
           <Row className='justify-content-center home-icons'>
             <Column xs='auto'>
-              <a href='https://github.com/ettariley' title='My Github' target="_blank"><i className="bi bi-github"></i></a>
+              <a href='https://github.com/ettariley' title='My Github' target="_blank"><i className="bi bi-github hi"></i></a>
             </Column>
             <Column xs='auto'>
-              <a href='https://www.linkedin.com/in/etta-r-baker/' title='My LinkedIn' target="_blank"><i className="bi bi-linkedin"></i></a>
+              <a href='https://www.linkedin.com/in/etta-r-baker/' title='My LinkedIn' target="_blank"><i className="bi bi-linkedin hi"></i></a>
             </Column>
             <Column xs='auto'>
-              <a href='https://1drv.ms/b/s!Anm4ZSZ5wNodnyvDupJezNxZv5Y6' download title='My Resume' target="_blank"><i className="bi bi-file-earmark-arrow-down-fill"></i></a>
+              <a href={require('../../assets/Etta_Haselden_Resume.pdf')} download title='My Resume' target="_blank"><i className="bi bi-file-earmark-arrow-down-fill hi"></i></a>
             </Column>
           </Row>
         </Container>
