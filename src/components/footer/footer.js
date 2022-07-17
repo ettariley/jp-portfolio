0ed312f517dc6881204356ee3fdf9475a9576c14
Etta Baker <etta.r.baker@gmail.com>
2022-07-17 13:33:24 -0400

    Cleaned up stylings

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,27 +8,25 @@ import './footer.css';
 function Footer() {
   return (
     <Container fluid className='footer'>
-      <Row className='footer-row justify-content-center align-items-center'>
-        <Column xs='auto' className='footer-icons'>
+      <Row className='p-2 justify-content-center align-items-center'>
+        <Column xs='auto'>
           <a href='mailto:ettahaselden@gmail.com' title='Email Me' target="_blank"><i class="bi fi bi-envelope-fill h2"></i></a>
         </Column>
-        <Column xs='auto' className='footer-icons'>
+        <Column xs='auto'>
           <a href='tel:423-438-7569' title='Call Me'><i class="bi fi bi-telephone-fill h2"></i></a>
         </Column>
-        <Column xs='auto' className='footer-icons'>
+        <Column xs='auto'>
           <a href={require('../../assets/Etta_Haselden_Resume.pdf')} download title='My Resume' target="_blank"><i className="bi bi-file-earmark-arrow-down-fill fi h2"></i></a>
         </Column>
-        <Column xs='auto' className='footer-icons'>
+        <Column xs='auto'>
           <a href='https://github.com/ettariley' title='My Github' target="_blank"><i className="bi fi bi-github h2"></i></a>
         </Column>
-        <Column xs='auto' className='footer-icons'>
+        <Column xs='auto'>
           <a href='https://www.linkedin.com/in/etta-r-baker/' title='My LinkedIn' target="_blank"><i className="bi fi bi-linkedin h2"></i></a>
         </Column>
       </Row>
-      <Row footer-row>
-        <Column>
-          <h6>Designed and developed by Me!</h6>
-        </Column>
+      <Row className='pb-1'>
+        <h6>Designed and developed by Me!</h6>
       </Row>
     </Container>
   );
