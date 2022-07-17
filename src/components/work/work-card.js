87705a8c45fabfd8013d956f9a8fccd344d7de43
Etta Baker <etta.r.baker@gmail.com>
2022-07-17 13:34:49 -0400

    Updated specific project info

--- src/components/work/work-card.js
+++ src/components/work/work-card.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import Column from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
 import './work.css';
 
 function WorkCard(props) {
@@ -11,34 +9,30 @@ function WorkCard(props) {
   const techList = tech.toString();
 
   return (
-      <Card className='work-card'>
-        <Card.Img variant='top' src={work.screenshot} />
-        <Card.Body>
-          <Card.Title>{work.title}</Card.Title>
-          <Card.Subtitle>{work.tagline}</Card.Subtitle>
-        </Card.Body>
-        <Card.Body>
-          <Card.Text className='m-0'>Technologies:</Card.Text>
-          <Card.Text>{techList}</Card.Text>
-        </Card.Body>
-        <Card.Body>
-          <Card.Text>{work.description}</Card.Text>
-        </Card.Body>
-        <Card.Body>
-          {work.github ? (
-            <Button variant="primary" className='m-2' href={work.github} target="_blank">Code</Button>
-          ) : null}
-          {work.demo ? (
-            <Button variant="primary" className='m-2' href={work.demo} target="_blank">Demo</Button>
-          ) : null}
-          {work.liveSite ? (
-            <Button variant="primary" className='m-2' href={work.liveSite} target="_blank">Website</Button>
-          ) : null}
-          {work.caseStudy ? (
-            <Button variant="primary" className='m-2' href='/case-study'>Case Study</Button>
-          ) : null}
-        </Card.Body>
-      </Card>    
+    <Card className='work-card'>
+      <Card.Img variant='top' src={work.screenshot} />
+      <Card.Body>
+        <Card.Title>{work.title}</Card.Title>
+        <Card.Subtitle>{work.tagline}</Card.Subtitle>
+        <Card.Text className='m-0 fs-5 pt-2'>Technologies:</Card.Text>
+        <Card.Text>{techList}</Card.Text>
+        <Card.Text>{work.description}</Card.Text>
+      </Card.Body>
+      <Card.Body>
+        {work.github ? (
+          <Button variant="primary" className='work-btn m-2' href={work.github} target="_blank">Code</Button>
+        ) : null}
+        {work.demo ? (
+          <Button variant="primary" className='work-btn m-2' href={work.demo} target="_blank">Demo</Button>
+        ) : null}
+        {work.liveSite ? (
+          <Button variant="primary" className='work-btn m-2' href={work.liveSite} target="_blank">Website</Button>
+        ) : null}
+        {work.caseStudy ? (
+          <Button variant="primary" className='work-btn m-2' href='/case-study'>Case Study</Button>
+        ) : null}
+      </Card.Body>
+    </Card>    
   );
 }
 
