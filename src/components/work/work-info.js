c84c8875ddbe1c9e040b35d57589205fec7d5244
Etta Baker <etta.r.baker@gmail.com>
2022-07-17 02:34:43 -0400

    Added project info and screenshots

---
+++ src/components/work/work-info.js
@@ -0,0 +1,102 @@
+const workInfo = [
+  {
+    title: 'Portfolio Site - React',
+    screenshot: require('../../assets/JP_Portfolio_Screenshot.png'),
+    tagline: 'Practice React Web App',
+    technologies: [
+      'React',
+      ' React Bootstrap',
+      ' Github & Github Pages',
+    ],
+    description: 'I created this portfolio site in July 2022 to get feedback for a portfolio site prior to the completion of the same project in Angular for the web development course. I used React & React Bootstrap to get a quick feel of the layout and design of the site.',
+    github: 'https://github.com/ettariley/jp-portfolio',
+    demo: 'https://ettariley.github.io/jp-portfolio/',
+  },
+  {
+    title: 'Chat',
+    screenshot: require('../../assets/Chat_Screenshot.png'),
+    tagline: 'React Native Mobile App',
+    technologies: [
+      'React Native',
+      ' Expo',
+      ' Google Firebase',
+    ],
+    description: 'I developed this project in July 2022 to get some experience with React Native. Working with Gifted Chat was so neat! The app is currently only available in development mode; instructions for downloading & deploying via Expo provided in README. Video demonstration of the app can be provided on request.',
+    github: 'https://github.com/ettariley/Chat-App',
+  },
+  {
+    title: 'Meet',
+    screenshot: require('../../assets/Meet_Screenshot.png'),
+    tagline: 'Serverless PWA Using Google API',
+    technologies: [
+      'AWS Lambda & Serverless',
+      ' Google Calendar API & OAuth',
+      ' Test-Driven Development Design',
+    ],
+    description: 'The aim of this project was to use test- and behavior-driven development designs. I worked backwards from user stories, to testing, to writing functional code. I implemented this in a progressive web app, which uses the Google Calendar API & OAuth. I even got to do some data analytics with the charts you see pictured! Developed in June 2022.',
+    github: 'https://github.com/ettariley/meet',
+  },
+  {
+    title: "Etta's Movie Database",
+    screenshot: require('../../assets/Screenshot_EMDB Movies_Case Study.png'),
+    tagline: 'Full-Stack Application - MERN Stack',
+    technologies: [
+      'React & Redux',
+      ' MongoDB & Mongoose',
+      ' Node.js & Express.js',
+    ],
+    description: 'Learn all the details about this project in the case study. Developed from April - June 2022',
+    caseStudy: true,
+  },
+  {
+    title: 'Pokedex App',
+    screenshot: require('../../assets/Pokedex_Screenshot.png'),
+    tagline: 'A JavaScript App Using an External API',
+    technologies: [
+      'JavaScript',
+      ' jQuery',
+      ' HTML, CSS, & Bootstrap',
+    ],
+    description: 'I made this project in March 2022 as my first JavaScript application. It uses the Pokedex API.',
+    github: 'https://github.com/ettariley/PokedexEtta',
+    demo: 'https://ettariley.github.io/PokedexEtta/',
+  },
+  {
+    title: 'To Do List',
+    screenshot: require('../../assets/To_Do_List_Screenshot.png'),
+    tagline: 'jQuery Application',
+    technologies: [
+      'tech 1',
+      ' tech 2',
+      ' tech 3',
+    ],
+    description: 'DESCRIPTION NEEDED',
+    github: 'https://github.com/ettariley/EttaToDoList',
+    demo: 'https://ettariley.github.io/EttaToDoList/',
+  },
+  {
+    title: 'Portfolio Site - Static Website',
+    screenshot: require('../../assets/First_Portfolio_Screenshot.png'),
+    tagline: 'Simple Website for Learning the Basics',
+    technologies: [
+      'HTML',
+      ' CSS',
+      ' JavaScript',
+    ],
+    description: 'DESCRIPTION NEEDED',
+    github: 'https://github.com/ettariley/PortfolioSite',
+    demo: 'https://ettariley.github.io/PortfolioSite/index.html',
+  },
+  {
+    title: 'Live Free Website',
+    screenshot: require('../../assets/Live_Free_Screenshot.png'),
+    tagline: 'Webflow Website',
+    technologies: [
+      'Webflow',
+    ],
+    description: 'This site was made in 2020 as a pro bono project for a social work colleague for her new nonprofit organization. While I did not use any original code, I enjoyed the design process for this site and find it a good example of my design skills and style. The nonprofit currently manages the content of the pages.',
+    liveSite: 'https://www.livefreeclaiborne.org/',
+  },
+];
+
+export { workInfo };
