827b0ce00ec201c7da493cb4bc4c2bad5a6eae46
Etta Baker <etta.r.baker@gmail.com>
2022-07-23 22:43:57 -0400

    Implemented scroll to top functionality

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from './components/scroll-to-top/scroll-to-top';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
